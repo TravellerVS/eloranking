e38c57f26b47bc90c74c0bf23e94a13ffe842ae3
Vedran Semenski <vedsemenski@gmail.com>
2020-03-16 23:16:45 +0100

    Initial commit

---
+++ src/test/java/com/vedransemenski/eloranking/ElorankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedransemenski.eloranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElorankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
